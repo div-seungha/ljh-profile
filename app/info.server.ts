5594d55654f3d6240bf57dbbcb18cc0837bb4641
div-seungha <sh.kim.x928@gmail.com>
2025-03-10 18:44:19 +0900

    Replace JSON const to envrionmental variables

--- app/info.server.ts
+++ app/info.server.ts
@@ -1,5 +1,4 @@
 import { google } from "googleapis";
-import credentials from "./credentials.json";
 
 type Entry = {
   order: string;
@@ -11,6 +10,20 @@ type Entry = {
 };
 
 type TransformedData = Record<string, Entry[]>;
+
+const credentials = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 export async function getSheetData() {
   const auth = new google.auth.GoogleAuth({
